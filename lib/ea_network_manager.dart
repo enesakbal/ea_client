425a16962773dcb71e0f507d177ea740b702c94f
Enes Akbal <enesakbal00@gmail.com>
2023-09-26 14:06:36 +0300

    ref: refactored ea network manager

--- lib/ea_network_manager.dart
+++ lib/ea_network_manager.dart
@@ -1,24 +1,25 @@
 // ignore_for_file: public_member_api_docs, sort_constructors_first
 import 'client_config.dart';
-import 'clients/dio_client.dart';
+import 'clients/dio/dio_client.dart';
+import 'clients/http/http_client.dart';
 import 'core/base/base_ea_client.dart';
 import 'core/enums/client_adapters.dart';
 
 class EANetworkManager {
   static late final ClientConfig _config;
 
-  static late final BaseEAClient _dioClient;
+  static late final BaseEAClient _client;
 
   EANetworkManager({required ClientConfig config}) {
     _config = config;
     switch (_config.adapter) {
       case ClientAdapters.DIO:
-        _dioClient = DioClient(config: _config);
+        _client = DioClient(config: _config);
       case ClientAdapters.HTTP:
-        _dioClient = DioClient(config: _config);
+        _client = HttpClient(config: _config);
         break;
     }
   }
 
-  BaseEAClient get client => _config.adapter == ClientAdapters.DIO ? _dioClient : _dioClient;
+  BaseEAClient get client => _client;
 }
